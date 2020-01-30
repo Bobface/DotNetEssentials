ac3746dc198e6bdde43388c99ac27e750eef384d
Lukas Nordin <lukas.nordin@bybrick.se>
2020-01-30 20:21:50 +0100

    Clipboard: add GTK support
    Based on https://github.com/stavroskasidis/XamarinClipboardPlugin/commit/b213a88d3d9d85d3600cd5084a53c670f51f0fb7

--- Xamarin.Essentials/Clipboard/Clipboard.netstandard.tvos.watchos.tizen.cs
+++ Xamarin.Essentials/Clipboard/Clipboard.netstandard.tvos.watchos.tizen.cs
@@ -4,6 +4,31 @@ namespace Xamarin.Essentials
 {
     public static partial class Clipboard
     {
+#if GTK
+        private static readonly Gdk.Atom clipboardAtom = Gdk.Atom.Intern("CLIPBOARD", false);
+
+        static Task PlatformSetTextAsync(string text)
+        {
+            var clipboard = Gtk.Clipboard.Get(clipboardAtom);
+            clipboard.Text = text;
+            return Task.FromResult(0);
+        }
+
+        static bool PlatformHasText
+        {
+            get
+            {
+                var clipboard = Gtk.Clipboard.Get(clipboardAtom);
+                return clipboard.WaitIsTextAvailable();
+            }
+        }
+
+        static Task<string> PlatformGetTextAsync()
+        {
+            var clipboard = Gtk.Clipboard.Get(clipboardAtom);
+            return Task.FromResult(clipboard.WaitForText());
+        }
+#else
         static Task PlatformSetTextAsync(string text)
             => throw ExceptionUtils.NotSupportedOrImplementedException;
 
